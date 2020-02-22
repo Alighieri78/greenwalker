a812515998dad92f81018080a9d5ed16dcbc4d93
aw88-uwf <42752030+aw88-uwf@users.noreply.github.com>
2020-02-22 15:11:44 -0600

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,11 @@
+const { Pool } = require('pg')
 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '',
+  post: 5432
+})
+
+module.exports = pool
