f483f3f13f4052166bcdbf57d4fc00a84f910315
aw88-uwf <42752030+aw88-uwf@users.noreply.github.com>
2020-03-03 09:25:49 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Validated Login Form</h1>
-      //<ValidatedLoginForm />
-    </div>
-  );
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
